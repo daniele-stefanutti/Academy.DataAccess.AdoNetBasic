ff1932bb95183a2750773b4268e589fab37dd809
Daniele Stefanutti <daniele.stefanutti@euris.it>
2024-04-22 19:11:55 +0200

    Add solutions for Exercise 1

---
+++ AdoNetBasic/Extensions/SqlCommandExtensions.cs
@@ -0,0 +1,75 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace AdoNetBasic.Extensions;
+
+internal static class SqlCommandExtensions
+{
+    public static IReadOnlyList<T> ExecuteReadCommand<T>(this SqlCommand command, SqlParameter parameter, Func<SqlDataReader, T> getResults)
+        => command.ExecuteReadCommand(new SqlParameter[] { parameter }, getResults);
+
+    public static IReadOnlyList<T> ExecuteReadCommand<T>(this SqlCommand command, SqlParameter[] parameters, Func<SqlDataReader, T> getResults)
+    {
+        using SqlConnection connection = new(Constants.ConnectionString);
+        connection.Open();
+
+        command.Connection = connection;
+        command.Parameters.AddRange(parameters);
+
+        using SqlDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection);
+
+        List<T> results = new();
+
+        while (dataReader.Read())
+            results.Add(getResults(dataReader));
+
+        return results;
+    }
+
+    public static int ExecuteWriteCommand(this SqlCommand command, SqlParameter parameter)
+        => command.ExecuteWriteCommand(new SqlParameter[] { parameter });
+
+    public static int ExecuteWriteCommand(this SqlCommand command, SqlParameter[] parameters)
+    {
+        using SqlConnection connection = new(Constants.ConnectionString);
+        connection.Open();
+
+        return command.ExecuteWriteCommand(connection, parameters);
+    }
+
+    public static int ExecuteWriteCommand(this SqlCommand command, IReadOnlyList<SqlParameter[]> parametersSets)
+    {
+        using SqlConnection connection = new(Constants.ConnectionString);
+        connection.Open();
+
+        using SqlTransaction transaction = connection.BeginTransaction();
+
+        try
+        {
+            command.Transaction = transaction;
+
+            int rowsAffected = 0;
+
+            foreach (SqlParameter[] parameters in parametersSets)
+                rowsAffected += command.ExecuteWriteCommand(connection, parameters);
+
+            transaction.Commit();
+            return rowsAffected;
+        }
+        catch (Exception)
+        {
+            transaction.Rollback();
+            throw;
+        }
+    }
+
+    private static int ExecuteWriteCommand(this SqlCommand command, SqlConnection connection, SqlParameter[] parameters)
+    {
+        command.Connection ??= connection;
+
+        foreach (SqlParameter parameter in parameters)
+            command.Parameters.CreateOrUpdateParameter(parameter);
+
+        return command.ExecuteNonQuery();
+    }
+}
--- AdoNetBasic/Repositories/AirportRepository.cs
+++ AdoNetBasic/Repositories/AirportRepository.cs
@@ -1,20 +1,76 @@
-﻿using AdoNetBasic.Models;
+﻿using AdoNetBasic.Extensions;
+using AdoNetBasic.Models;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace AdoNetBasic.Repositories;
 
 internal class AirportRepository : IAirportRepository
 {
+    #region QUERIES
+
+    private const string SelectCommandText = @$"
+        SELECT * FROM {nameof(Airport)}
+    ";
+
+    private const string InsertCommandText = @$"
+        INSERT INTO {nameof(Airport)}
+        (
+            [{nameof(Airport.AirportCode)}],
+            [{nameof(Airport.AirportName)}],
+            [{nameof(Airport.ContactNo)}],
+            [{nameof(Airport.Longitude)}],
+            [{nameof(Airport.Latitude)}],
+            [{nameof(Airport.CountryCode)}]
+        )
+        VALUES
+        (
+            @{nameof(Airport.AirportCode)},
+            @{nameof(Airport.AirportName)},
+            @{nameof(Airport.ContactNo)},
+            @{nameof(Airport.Longitude)},
+            @{nameof(Airport.Latitude)},
+            @{nameof(Airport.CountryCode)}
+        );";
+
+    private const string UpdateCommandText = $@"
+        UPDATE {nameof(Airport)}
+        SET
+            [{nameof(Airport.AirportName)}] = @{nameof(Airport.AirportName)},
+            [{nameof(Airport.ContactNo)}] = @{nameof(Airport.ContactNo)},
+            [{nameof(Airport.Longitude)}] = @{nameof(Airport.Longitude)},
+            [{nameof(Airport.Latitude)}] = @{nameof(Airport.Latitude)},
+            [{nameof(Airport.CountryCode)}] = @{nameof(Airport.CountryCode)}
+        WHERE [{nameof(Airport.AirportCode)}] = @{nameof(Airport.AirportCode)};";
+
+    private const string DeleteCommandText = $@"
+        DELETE FROM {nameof(Airport)}";
+
+    #endregion
+
     #region READ
 
     /// <remarks>
     /// Please, implement this method!
     /// </remarks>
-    public Airport? GetByAirportCode(string airportCode) => throw new NotImplementedException();
+    public Airport? GetByAirportCode(string airportCode)
+    {
+        using SqlCommand command = new($"{SelectCommandText} WHERE {nameof(Airport.AirportCode)} = @{nameof(Airport.AirportCode)}");
+        SqlParameter parameter = new($"@{nameof(Airport.AirportCode)}", airportCode);
+
+        return command.ExecuteReadCommand(parameter, ParseAirportFromQueryResult).FirstOrDefault();
+    }
 
     /// <remarks>
     /// Please, implement this method!
     /// </remarks>
-    public IReadOnlyList<Airport> GetByCountryCode(string countryCode) => throw new NotImplementedException();
+    public IReadOnlyList<Airport> GetByCountryCode(string countryCode)
+    {
+        using SqlCommand command = new($"{SelectCommandText} WHERE {nameof(Airport.CountryCode)} = @{nameof(Airport.CountryCode)}");
+        SqlParameter parameter = new($"@{nameof(Airport.CountryCode)}", countryCode);
+
+        return command.ExecuteReadCommand(parameter, ParseAirportFromQueryResult);
+    }
 
     /// <remarks>
     /// Please, implement this method!
@@ -30,7 +86,21 @@ internal class AirportRepository : IAirportRepository
     /// 
     /// </remarks>
     public IReadOnlyList<Airport> GetBySquareArea(double northWestLongitude, double northWestLatitude, double southEastLongitude, double southEastLatitude)
-        => throw new NotImplementedException();
+    {
+        using SqlCommand command = new(@$"
+            {SelectCommandText}
+            WHERE {nameof(Airport.Longitude)} >= @NorthWestLongitude and {nameof(Airport.Longitude)} <= @SouthEastLongitude and 
+                  {nameof(Airport.Latitude)} >= @NorthWestLatitude and {nameof(Airport.Latitude)} <= @SouthEastLatitude");
+        SqlParameter[] parameters = new SqlParameter[]
+        {
+            new("@NorthWestLongitude", northWestLongitude),
+            new("@NorthWestLatitude", northWestLatitude),
+            new("@SouthEastLongitude", southEastLongitude),
+            new("@SouthEastLatitude", southEastLatitude)
+        };
+
+        return command.ExecuteReadCommand(parameters, ParseAirportFromQueryResult);
+    }
 
     #endregion
 
@@ -40,13 +110,25 @@ internal class AirportRepository : IAirportRepository
     /// Please, implement this method!
     /// </remarks>
     /// <returns>Number of affected rows</returns>
-    public int Add(Airport airport) => throw new NotImplementedException();
+    public int Add(Airport airport)
+    {
+        using SqlCommand command = new(InsertCommandText);
+        SqlParameter[] parameters = GetAirportParameters(airport);
+
+        return command.ExecuteWriteCommand(parameters);
+    }
 
     /// <remarks>
     /// Please, implement this method!
     /// </remarks>
     /// <returns>Number of affected rows</returns>
-    public int AddRange(IReadOnlyList<Airport> airports) => throw new NotImplementedException();
+    public int AddRange(IReadOnlyList<Airport> airports)
+    {
+        using SqlCommand command = new(InsertCommandText);
+        IReadOnlyList<SqlParameter[]> parametersSet = airports.Select(a => GetAirportParameters(a)).ToList();
+
+        return command.ExecuteWriteCommand(parametersSet);
+    }
 
     #endregion
 
@@ -56,7 +138,13 @@ internal class AirportRepository : IAirportRepository
     /// Please, implement this method!
     /// </remarks>
     /// <returns>Number of affected rows</returns>
-    public int Update(Airport airport) => throw new NotImplementedException();
+    public int Update(Airport airport)
+    {
+        using SqlCommand command = new(UpdateCommandText);
+        SqlParameter[] parameters = GetAirportParameters(airport);
+
+        return command.ExecuteWriteCommand(parameters);
+    }
 
     #endregion
 
@@ -66,13 +154,51 @@ internal class AirportRepository : IAirportRepository
     /// Please, implement this method!
     /// </remarks>
     /// <returns>Number of affected rows</returns>
-    public int DeleteByAirportCode(string airportCode) => throw new NotImplementedException();
+    public int DeleteByAirportCode(string airportCode)
+    {
+        using SqlCommand command = new($"{DeleteCommandText} WHERE {nameof(Airport.AirportCode)} = @{nameof(Airport.AirportCode)}");
+        SqlParameter parameter = new(nameof(Airport.AirportCode), airportCode);
+
+        return command.ExecuteWriteCommand(parameter);
+    }
 
     /// <remarks>
     /// Please, implement this method!
     /// </remarks>
     /// <returns>Number of affected rows</returns>
-    public int DeleteByCountryCode(string countryCode) => throw new NotImplementedException();
+    public int DeleteByCountryCode(string countryCode)
+    {
+        using SqlCommand command = new($"{DeleteCommandText} WHERE {nameof(Airport.CountryCode)} = @{nameof(Airport.CountryCode)}");
+        SqlParameter parameter = new(nameof(Airport.CountryCode), countryCode);
+
+        return command.ExecuteWriteCommand(parameter);
+    }
+
+    #endregion
+
+    #region Locals
+
+    private static Airport ParseAirportFromQueryResult(SqlDataReader dataReader)
+        => new()
+        {
+            AirportCode = dataReader.GetString(nameof(Airport.AirportCode)),
+            AirportName = dataReader.GetString(nameof(Airport.AirportName)),
+            ContactNo = dataReader.GetDecimal(nameof(Airport.ContactNo)),
+            Longitude = dataReader.GetDouble(nameof(Airport.Longitude)),
+            Latitude = dataReader.GetDouble(nameof(Airport.Latitude)),
+            CountryCode = dataReader.GetString(nameof(Airport.CountryCode))
+        };
+
+    private static SqlParameter[] GetAirportParameters(Airport airport)
+        => new SqlParameter[]
+        {
+            new(nameof(Airport.AirportCode), airport.AirportCode),
+            new(nameof(Airport.AirportName), airport.AirportName),
+            new(nameof(Airport.ContactNo), airport.ContactNo),
+            new(nameof(Airport.Longitude), airport.Longitude),
+            new(nameof(Airport.Latitude), airport.Latitude),
+            new(nameof(Airport.CountryCode), airport.CountryCode)
+        };
 
     #endregion
 }

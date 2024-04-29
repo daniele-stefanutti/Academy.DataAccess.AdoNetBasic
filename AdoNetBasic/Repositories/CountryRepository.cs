4de891cb45277b8a019ac0b7b877816f42434149
Daniele Stefanutti <daniele.stefanutti@euris.it>
2024-04-29 15:18:13 +0200

    Minor fixes

--- AdoNetBasic/Repositories/CountryRepository.cs
+++ AdoNetBasic/Repositories/CountryRepository.cs
@@ -1,7 +1,4 @@
-﻿using AdoNetBasic.Extensions;
-using AdoNetBasic.Models;
-using System.Data;
-using System.Data.SqlClient;
+﻿using AdoNetBasic.Models;
 
 namespace AdoNetBasic.Repositories;
 
--- AdoNetBasic/Repositories/FlightInstanceRepository.cs
+++ AdoNetBasic/Repositories/FlightInstanceRepository.cs
@@ -1,6 +1,4 @@
 ﻿using AdoNetBasic.Models;
-using System.Data;
-using System.Data.SqlClient;
 
 namespace AdoNetBasic.Repositories;
 
@@ -9,39 +7,10 @@ namespace AdoNetBasic.Repositories;
 /// </remarks>
 public class FlightInstanceRepository : IFlightInstanceRepository
 {
-    #region QUERIES
-
-    private const string SelectCommandText = @$"
-        SELECT * FROM {nameof(FlightInstance)}
-    ";
-
-    private const string SelectByInstanceIdCommandText = @$"
-        {SelectCommandText} WHERE {nameof(FlightInstance.InstanceId)} = @{nameof(FlightInstance.InstanceId)}
-    ";
-
-    #endregion
-
     #region READ
 
     public async Task<FlightInstance?> GetByInstanceIdAsync(int instanceId)
-    {
-        using SqlCommand command = new(SelectByInstanceIdCommandText);
-        SqlParameter parameter = new($"@{nameof(FlightInstance.InstanceId)}", instanceId);
-
-        return (await command.ExecuteReadCommandAsync(parameter, ParseFlightInstanceFromQueryResult)).FirstOrDefault();
-    }
-
-    #endregion
-
-    #region Locals
-
-    private static FlightInstance ParseFlightInstanceFromQueryResult(SqlDataReader dataReader)
-        => new()
-        {
-            InstanceId = dataReader.GetInt32(nameof(FlightInstance.InstanceId)),
-            CoPilotAboardId = dataReader.GetInt32(nameof(FlightInstance.CoPilotAboardId)),
-            DateTimeArrive = dataReader.GetDateTime(nameof(FlightInstance.DateTimeArrive))
-        };
+        => throw new NotImplementedException();
 
     #endregion
 }
